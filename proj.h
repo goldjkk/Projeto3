68ae116f4ba388b080a49db72e280777cf890f29
Matheus Concon <142353640+goldjkk@users.noreply.github.com>
2023-11-12 17:34:12 -0300

    Create proj.h

---
+++ proj.h
@@ -0,0 +1,52 @@
+#ifndef PROJ_H
+#define PROJ_H
+
+#define MAX_TAREFAS 100
+#define MAX_DESCRICAO 300
+#define MAX_CATEGORIA 100
+
+// Enumeração para representar o estado da tarefa
+enum Estado {
+    NAO_INICIADO,
+    EM_ANDAMENTO,
+    COMPLETO
+};
+
+// Estrutura que representa uma tarefa
+struct Tarefa {
+    int prioridade;
+    char descricao[MAX_DESCRICAO];
+    char categoria[MAX_CATEGORIA];
+    enum Estado estado;
+};
+
+
+void cadastrarTarefa(struct Tarefa tarefas[], int *numTarefas);
+
+
+void listarTarefas(struct Tarefa tarefas[], int numTarefas);
+
+
+void deletarTarefa(struct Tarefa tarefas[], int *numTarefas);
+
+
+void alterarTarefa(struct Tarefa tarefas[], int numTarefas);
+
+void filtrarPorPrioridade(struct Tarefa tarefas[], int numTarefas);
+
+
+void filtrarPorEstado(struct Tarefa tarefas[], int numTarefas);
+
+
+void filtrarPorCategoria(struct Tarefa tarefas[], int numTarefas);
+
+
+void filtrarPorPrioridadeECategoria(struct Tarefa tarefas[], int numTarefas);
+
+void exportarPorPrioridade(struct Tarefa tarefas[], int numTarefas);
+
+void exportarPorCategoria(struct Tarefa tarefas[], int numTarefas);
+
+void exportarPorPrioridadeECategoria(struct Tarefa tarefas[], int numTarefas);
+
+#endif // PROJ_H
